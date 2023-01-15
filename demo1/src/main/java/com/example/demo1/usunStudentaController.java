01885dc6c5aab232aeb565b8d2154cdfb7134931
Lukasz-Strugarek <272727@student.pwr.edu.pl>
2023-01-15 12:17:02 +0100

    usuwanie1

--- demo1/src/main/java/com/example/demo1/usunStudentaController.java
+++ demo1/src/main/java/com/example/demo1/usunStudentaController.java
@@ -1,22 +1,33 @@
 package com.example.demo1;
 
 import Uzytkownicy.Student;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import Dane.*;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.EventObject;
+import java.util.ResourceBundle;
 
-public class usunStudentaController {
+public class usunStudentaController implements Initializable {
 
     @FXML
     private Button returnButton;
@@ -26,6 +37,8 @@ public class usunStudentaController {
     private TextField usunietyIndeks;
     @FXML
     private Label uwaga;
+    @FXML
+    private VBox studenciBox;
 
     public void usunStudenta(Button usuwanie)
     {
@@ -71,5 +84,56 @@ public class usunStudentaController {
             uwaga.setVisible(true);
         }
         Dane.zapisStudentow(listaStudentow);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        int i = 0;
+        ArrayList<Student> studenci = null;
+        try {
+            studenci = Dane.odczytStudentow();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        if (studenci == null) {
+            System.out.println("Nie udało się załadować studentów");
+        }else {
+            for(Student student: studenci) {
+                HBox hBox = new HBox();
+                hBox.setPrefSize(35, 598);
+                Text imie = new Text(student.getImie());
+                HBox.setMargin(imie, new Insets(8, 0, 0, 10));
+                imie.setWrappingWidth(113.21630859375);
+                Text nazwisko = new Text(student.getNazwisko());
+                nazwisko.setWrappingWidth(137.21630859375);
+                HBox.setMargin(nazwisko, new Insets(8, 0, 0, 0));
+                Text indeks = new Text(student.getNr_indeksu());
+                indeks.setWrappingWidth(199.21630859375);
+                HBox.setMargin(indeks, new Insets(8, 0, 0, 0));
+                Button usuwanie = new Button("Usuń");
+                usuwanie.setPrefSize(12, 109);
+                usuwanie.setMinWidth(109);
+                usuwanie.setMaxHeight(12);
+                HBox.setMargin(usuwanie, new Insets(2, 0, 0, 0));
+                usuwanie.setTextFill(Color.rgb(255, 255, 255));
+                usuwanie.setBackground(new Background(new BackgroundFill(Color.rgb(0, 0, 187), null, null)));
+                usuwanie.setFont(new Font("Consolas", 14));
+                hBox.getChildren().add(imie);
+                hBox.getChildren().add(nazwisko);
+                hBox.getChildren().add(indeks);
+                hBox.getChildren().add(usuwanie);
+                hBox.setMaxHeight(35);
+                hBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,null, new BorderWidths(1.0))));
+                studenciBox.getChildren().add(hBox);
+            }
+        }
+
+
+    }
+
+    public void usunStudentaAction(){
+
     }
 }
