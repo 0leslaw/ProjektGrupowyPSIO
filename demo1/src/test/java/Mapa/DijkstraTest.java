b4b9f004087d2e35954d0629fe85ab2942806614
0leslaw <272736@student.pwr.edu.pl>
2023-01-21 20:41:21 +0100

    Update DijkstraTest.java

--- demo1/src/test/java/Mapa/DijkstraTest.java
+++ demo1/src/test/java/Mapa/DijkstraTest.java
@@ -8,6 +8,23 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DijkstraTest {
+    @Test
+    public void znajdzDroge(){
+        ArrayList<Wezel> wlist = new ArrayList<>();
+        Wezel[][] wTab = new Wezel[10][10];
+        Wezel roboczy;
+        for(int i =0;i<10;i++)
+            for(int j =0;j<10;j++) {
+                wlist.add(new Wezel(Integer.toString(i) + "x" + Integer.toString(j), j * 10, i * 10));
+                wTab[i][j] = wlist.get(wlist.size()-1);
+            }
+
+        for(int i =0;i<10;i++)
+            for(int j =0;j<10;j++){
+                if()
+            }
+
+    }
 
     @Test
     void TestZawo() {
