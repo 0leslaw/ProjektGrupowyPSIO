e1436d967445b15496ef22ad527fcd96ea2898c7
0leslaw <272736@student.pwr.edu.pl>
2023-01-21 19:27:29 +0100

    Testy 1

---
+++ demo1/src/test/java/TestKalendarza.java
@@ -0,0 +1,26 @@
+import Obserwowani.Kalendarz;
+import PakietWydarzenie.Wydarzenie;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class TestKalendarza {
+
+    @Test
+    public void stworzListeWydarzenNaDzien(){
+
+        //dane
+        Kalendarz kalendarz = new Kalendarz(new ArrayList<Wydarzenie>());
+        //dla pustej listy
+        //ma zajsc
+        for(int i = 0;i < 7;i++)
+        Assertions.assertEquals(0,kalendarz.stworzListeWydarzenNaDzien(
+                Kalendarz.zwracaDateNajbUbieglegoPoniedzialku(LocalDateTime.now()),i).size());
+
+        for(int i=0;i<10;i++)
+        kalendarz.dodajWydarzenieDoKalendarza(new Wydarzenie());
+    }
+
+}
